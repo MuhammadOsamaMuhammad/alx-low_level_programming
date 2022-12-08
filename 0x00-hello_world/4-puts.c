8f0c29a13c7dec825e50b79bd36e01b8f76e83fc
MuhammadOsamaMuhammad <mohamedosama1810@gmail.com>
2022-12-08 06:43:07 -0800

    s

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,6 @@
 #include <stdio.h>
-
-/*
- * Description main: entry point 
- * return 0
- */
-
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+puts("\"Programming is like building a multilingual puzzle");          
+return (0)
 }
