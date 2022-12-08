68a7e7dbd55b61ffad69c2a6c658551817b4bc7a
MuhammadOsamaMuhammad <mohamedosama1810@gmail.com>
2022-12-08 06:30:29 -0800

    tst

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /*
  * main always returns 0
  * puts "prgramming is like building a multilingual puzzle
- * */
+ */
 int main(void){
 puts("\"Programming is like building a multilingual puzzle");
 return 0;
