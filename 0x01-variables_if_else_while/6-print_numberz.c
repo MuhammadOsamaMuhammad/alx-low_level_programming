160e2e7ceb07419f817554ba34c2224c11c9b15e
MuhammadOsamaMuhammad <mohamedosama1810@gmail.com>
2022-12-09 08:39:50 -0800

    s

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
