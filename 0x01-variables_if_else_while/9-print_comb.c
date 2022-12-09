eecbb462218a72217819c80784b7fb17806cce30
MuhammadOsamaMuhammad <mohamedosama1810@gmail.com>
2022-12-09 08:52:38 -0800

    s

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar((i % 10) + '0');
+		if (i ==9)
+		{ 
+			continue;
+		}
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
