1e837617df78ba3ced5fbb51fd0209eb87d95966
MuhammadOsamaMuhammad <mohamedosama1810@gmail.com>
2022-12-09 08:32:05 -0800

    s

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
