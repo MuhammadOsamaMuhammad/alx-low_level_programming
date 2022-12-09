1bdfa5392c5a427a070ec08ccc82b1578f9981a8
MuhammadOsamaMuhammad <mohamedosama1810@gmail.com>
2022-12-09 06:50:52 -0800

    + or 0 or -

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,9 +14,9 @@ int main(void)
 
 {
 	int n;
-	
+
 	srand(time(0));
-	
+
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
@@ -27,6 +27,6 @@ int main(void)
 
 	else
 		printf("%d is zero\n", n);
-	
+
 	return (0);
 }
