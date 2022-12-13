b1b076e42b06f076a03635804a596a59b139054f
MuhammadOsamaMuhammad <mohamedosama1810@gmail.com>
2022-12-13 11:44:15 -0800

    print alphabet 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Return: Always 0
+ *
+ * description: print alphabet 10x
+ */
+
+
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		print_alphabet();
+		i++;
+	}
+	_putchar("\n");
+}
