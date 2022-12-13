a282413012be59baf1bcb330be421bd225aecdd9
MuhammadOsamaMuhammad <mohamedosama1810@gmail.com>
2022-12-13 13:20:17 -0800

    test

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ *  time_table - Entry point
+ *
+ * Return: void
+ *
+ *
+ * description: print the time table from 0 to 9
+ */
+
+
+void times_table(void)
+{
+	int n, r, f;
+
+	for (n = 0; n <10; n++)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		for (r = 0; r < 9; r++)
+		{
+			f = n * r;
+			if (f / 10 > 0)
+			{
+				_putchar((f / 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+			_putchar((d / 10) + '0');
+
+			if (f < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+	_putchar('\n');
+
+
+}
