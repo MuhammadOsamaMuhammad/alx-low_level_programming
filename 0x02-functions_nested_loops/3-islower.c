5e0ef3c6d691231314b0703e4905f144c7a42b5f
MuhammadOsamaMuhammad <mohamedosama1810@gmail.com>
2022-12-13 12:15:28 -0800

    test

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * islower - Entry point
+ *
+ * Return: 0 or 1
+ *
+ * description: print lowercase alphabet
+ */
+
+
+int print_alphabet(int a)
+{
+	if (a >= 'a' && a <= 'z')
+	{
+		return (1);
+	}
+	return (1);
+}
