0de7d64b7f5173a691b04752ee5b6fc5c21173be
MuhammadOsamaMuhammad <mohamedosama1810@gmail.com>
2022-12-13 13:57:54 -0800

    test

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ *
+ * @i: first param
+ *
+ * @s: second param
+ * Return: int
+ *
+ * description: print sum of two numbers
+ */
+
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		while (n <= 98)
+		{
+			_putchar(n + '0');
+			if (n != 95)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			n++;
+		}
+	}
+	else if (n > 98)
+	{
+		while (n >= 98)
+		{
+			_putchar(n + '0');
+			if (n != 95)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			n--;
+		}
+	}
+	else
+	{
+		_putchar('9');
+		_putchar('8');
+	}
+	_putchar('\n');
+
+}
