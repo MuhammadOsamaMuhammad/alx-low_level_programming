31d59d6316f0d1d8afd341b263f7c9a2be0bfadd
MuhammadOsamaMuhammad <mohamedosama1810@gmail.com>
2022-12-15 04:09:52 -0800

    print numbers from 0 to 9

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,9 +11,14 @@ void more_numbers(void)
 
 	for (j = 0; j < 10; j++)
 	{
+
 		for (i = 0; i < 15; i++)
 		{
-			_putchar(i + '0');
+			if (j >= 10)
+			{
+				_putchar((i / 10) + '0');
+			}
+			_putchar((i % 10) + '0');
 		}
 		_putchar('\n');
 	}
