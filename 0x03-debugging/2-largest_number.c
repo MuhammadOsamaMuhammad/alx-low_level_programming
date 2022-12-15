02fa41e8f858c536f1c497d299ef09ffbe51d82b
MuhammadOsamaMuhammad <105750421+MuhammadOsamaMuhammad@users.noreply.github.com>
2022-12-15 23:41:50 +0200

    Create 2-largest_number.c

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @a: input int
+ * @b: input int
+ * @c: input int
+ * Return: largest number of three inputs
+ */
+
+int largest_number(int a, int b, int c)
+{
+  if (a > b && a > c)
+  {
+     return (a);
+  }
+  else if (b > c)
+  {
+    return (b);
+  }
+    largest = c;
+}
