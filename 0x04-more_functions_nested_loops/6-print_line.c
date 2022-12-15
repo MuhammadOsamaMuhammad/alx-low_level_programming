923571ce218e9a1b465675e6d51a58de5be97684
MuhammadOsamaMuhammad <mohamedosama1810@gmail.com>
2022-12-15 04:30:20 -0800

    print numbers from 0 to 9

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_line -  a function that print straight line
+ * @n: input
+ * Return: void
+ */
+void print_line(int n)
+{
+	while (n > 0)
+	{
+		_putchar('_');
+		n--;
+	}
+}
