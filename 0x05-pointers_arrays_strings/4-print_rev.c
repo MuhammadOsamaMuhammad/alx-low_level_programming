e75e56f5c88279f777991fce6b02c793f33e83c9
MuhammadOsamaMuhammad <mohamedosama1810@gmail.com>
2022-12-19 13:53:39 -0800

    test

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_rev_ - function that returns print reverse string
+ * @s: pointer
+ * Return: void
+ */
+
+int void print_rev(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; ++i)
+	{
+	}
+	while (i > 0)
+	{
+		_putchar(s[i]);
+	}
+}
