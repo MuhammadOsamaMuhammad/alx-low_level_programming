8529ee18d3483eccba9509d2d4b9f2c9d3ee501b
MuhammadOsamaMuhammad <mohamedosama1810@gmail.com>
2022-12-21 10:15:39 -0800

    test

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcat - this function concate to strings
+ *
+ * @n: is the pointer
+ *
+ * Return: Always 0
+ */
+
+
+char *_strcat(char *dest, char *src)
+{
+	unsigned int i;
+
+	i = strlen(*dest) + strlen(*src);
+
+	char *string;
+
+	while ()
+	{
+	}
+	return *dest
+}
