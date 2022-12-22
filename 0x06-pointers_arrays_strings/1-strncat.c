406e0f92a97c8afe83e8180d59dfe4286dba6b45
MuhammadOsamaMuhammad <mohamedosama1810@gmail.com>
2022-12-22 14:11:37 -0800

    tse

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,22 +14,21 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int l1;                                                                               
-        int l2;                                                                               
-        int i;                                                                                
-                                                                                              
-        l1 = 0;                                                                               
-        l2 = 0;                                                                               
-                                                                                              
-        while (src[l1] != '\0')                                                               
-                l1++;                                                                         
-                                                                                              
-        while (dest[l2] != '\0')                                                              
-                l2++;                                                                         
-                                                                                              
-        for (i = 0; i <= l1; i++)                                                             
-        {                                                                                     
-                dest[l2 + i] = src[i];                                                        
-        }                                                                                     
+	int i;
+        int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
         return (dest);
 }
