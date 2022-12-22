a58e879daa3c37b665fee599f7fef7dca5c2b4f6
MuhammadOsamaMuhammad <mohamedosama1810@gmail.com>
2022-12-22 00:37:28 -0800

    test

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _strncat - function to concate two strings
+ *
+ * @dest: first string
+ *
+ * @src: second string
+ *
+ * @n: number of bytes in the second string
+ *
+ * Return: pointer to the first string
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int l1;
+	int l2;
+	int i;
+
+	l1 = 0;
+	l2 = 0;
+
+	while (src[l1] != '\0')
+		l1++;
+
+	while (dest[l2] != '\0')
+		l2++;
+
+	for (i = 0; i <= l1; i++)
+	{
+		dest[l2 + i] = src[i];
+	}
+	return (dest);
+}
