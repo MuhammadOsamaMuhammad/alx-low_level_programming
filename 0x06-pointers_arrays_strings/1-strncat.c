ac7495de35886b34fcc6eb2c7fd6b30665711a31
MuhammadOsamaMuhammad <mohamedosama1810@gmail.com>
2022-12-22 04:06:09 -0800

    test

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,22 +9,22 @@
  *
  * @n: number of bytes in the second string
  *
- * Return: pointer to the first string
+ * Return: pointer to the first string @dest
 */
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int l2;
+	int l;
 	int i;
 
 	l2 = 0;
 
-	while (dest[l2] != '\0')
-		l2++;
+	while (dest[l] != '\0')
+		l++;
 
 	for (i = 0; i < n; i++)
 	{
-		dest[l2 + i] = src[i];
+		dest[l + i] = src[i];
 	}
-	return (*dest);
+	return (dest);
 }
