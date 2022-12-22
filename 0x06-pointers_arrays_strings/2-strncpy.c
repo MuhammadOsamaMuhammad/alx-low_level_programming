8a0cf73fa0fca349a0518ccd1a341cf863e88ab3
MuhammadOsamaMuhammad <mohamedosama1810@gmail.com>
2022-12-22 04:14:49 -0800

    test

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strncpy - function that copies a string.
+ *
+ * @dest: first string
+ *
+ * @src: second string
+ *
+ * Return: pointer to the first string
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	               size_t dest_len = strlen(dest);                             
+
+		                      size_t i;                                                   
+
+				                                                                                 
+
+				                     for (i = 0 ; i < n &}
