0ac5ab646862848a835d7642121e82abe2dbeabb
MuhammadOsamaMuhammad <105750421+MuhammadOsamaMuhammad@users.noreply.github.com>
2022-12-23 00:04:44 +0200

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,28 +14,22 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int len1 = 0; /* string length for src */
-	int len2 = 0; /* string length for dest */
-	int i;
-
-	while (src[len1] != '\0')
-		len1++;
-
-	while (dest[len2] != '\0')
-		len2++;
-
-	for (i = 0; i < n && src[i] != '\0'; i++)
-	{
-		if (i < len1)
-		{
-			dest[len2 + i] = src[i];
-		}
-		else
-		{
-			dest[len2 + i] = '\0';
-			break;
-		}
-	}
-// 	dest[l + i] = '\0';
-	return (dest);
+	int l1;                                                                               
+        int l2;                                                                               
+        int i;                                                                                
+                                                                                              
+        l1 = 0;                                                                               
+        l2 = 0;                                                                               
+                                                                                              
+        while (src[l1] != '\0')                                                               
+                l1++;                                                                         
+                                                                                              
+        while (dest[l2] != '\0')                                                              
+                l2++;                                                                         
+                                                                                              
+        for (i = 0; i <= l1; i++)                                                             
+        {                                                                                     
+                dest[l2 + i] = src[i];                                                        
+        }                                                                                     
+        return (dest);
 }
