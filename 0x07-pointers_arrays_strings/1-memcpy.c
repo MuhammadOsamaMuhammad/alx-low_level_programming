c4304e04adcc312b8c53ee0890e3fe856c2acb6b
MuhammadOsamaMuhammad <105750421+MuhammadOsamaMuhammad@users.noreply.github.com>
2023-01-03 00:56:10 +0200

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,15 +1,11 @@
 #include "main.h"
-
 /**
- * _memcpy - Entry point
+ *_memcpy - a function that copies memory area
+ *@dest: memory where is stored
+ *@src: memory where is copied
+ *@n: number of bytes
  *
- * @dest: input
- *
- * @src: input
- *
- * @n: input
- *
- * Return pointer
+ *Return: copied memory with n byted changed
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
