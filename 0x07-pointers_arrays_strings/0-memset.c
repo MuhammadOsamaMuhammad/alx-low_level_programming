97607bc32016d16913813dbc08de5d267735fdf6
MuhammadOsamaMuhammad <mohamedosama1810@gmail.com>
2023-01-02 14:38:37 -0800

    _memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - Entry point
+ * @s: pinted destination
+ * @b: constant byte
+ * @n: byte
+ * Return 0
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
