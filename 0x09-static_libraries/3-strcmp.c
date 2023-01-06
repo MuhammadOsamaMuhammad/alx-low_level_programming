ffb297c4a7fce64bbd7fb9d0e268ac68513daad3
MuhammadOsamaMuhammad <mohamedosama1810@gmail.com>
2023-01-06 07:34:35 -0800

    test

--- 0x09-static_libraries/3-strcmp.c
+++ 0x09-static_libraries/3-strcmp.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
- * _strcmp - compare string values
- * @s1: input value
- * @s2: input value
- *
- * Return: s1[i] - s2[i]
+* _strcmp - main point
+* @s1: input value
+* @s2: input value
+*
+* Return: s1[i] - s2[i]
  */
 int _strcmp(char *s1, char *s2)
 {
 	int i;
- 
+
 	i = 0;
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
@@ -17,7 +17,6 @@ int _strcmp(char *s1, char *s2)
 		{
 			return (s1[i] - s2[i]);
 		}
-	i++;
 	}
 	return (0);
 }
