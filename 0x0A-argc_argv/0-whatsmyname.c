6b809c9712bbb8d0874a508dd65f0497b18d281e
MuhammadOsamaMuhammad <mohamedosama1810@gmail.com>
2023-01-06 14:27:06 -0800

    test

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main -Prints the name of the program
+ * @argc: number of the params to the program
+ * @argv: array of the program prarms
+ * Return: returns 0 when succsess
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
