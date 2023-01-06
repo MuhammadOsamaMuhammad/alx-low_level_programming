a7e94c371800c36afd73b3b97c25af035c10a150
MuhammadOsamaMuhammad <mohamedosama1810@gmail.com>
2023-01-06 15:02:39 -0800

    tset

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main -Print the arguments
+ * @argc: number of the params to the program
+ * @argv: array of the program prarms
+:x
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
