f80d5b90706093d96d9b631dde1ec2d3581c905d
MuhammadOsamaMuhammad <mohamedosama1810@gmail.com>
2023-01-06 14:43:42 -0800

    test

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Prints the number of args
+ * @argc: number of args
+ * @argv: array of args
+ * Retrun: 0
+ */
+
+
+int main(int argc, char *argv[])
+{
+
+	if (argc == 1)
+		printf("%d\n", argc - 1);
+	else
+		printf("%d\n", argc - 1);
+	return (0);
+}
