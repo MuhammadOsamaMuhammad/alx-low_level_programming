1688277185ff10260d4652d689a568b0038f3376
MuhammadOsamaMuhammad <105750421+MuhammadOsamaMuhammad@users.noreply.github.com>
2023-01-06 17:14:14 +0200

    Update 0-strcat.c

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -10,6 +10,7 @@ char *_strcat(char *dest, char *src)
 	int i;
 	int j;
 	i = 0;
+	
 	while (dest[i] != '\0')
 	{
 		i++;
@@ -20,7 +21,7 @@ char *_strcat(char *dest, char *src)
 		dest[i] = src[j];
 		i++;
 		j++;
-	}		
+	}
 	dest[i] = '\0';
 	return (dest);
 }
