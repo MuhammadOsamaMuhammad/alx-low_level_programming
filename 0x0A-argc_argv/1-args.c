4ceaaf6c6576237273146a23ddb28e730f85824a
MuhammadOsamaMuhammad <105750421+MuhammadOsamaMuhammad@users.noreply.github.com>
2023-01-07 00:53:37 +0200

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,17 +1,19 @@
 #include "main.h"
 #include <stdio.h>
 
-/**
- * main - Prints the number of args
- * @argc: number of args
- * @argv: array of args
- * Retrun: 0
- */
-
-
 int main(int argc, char *argv[])
 {
-	printf("%d\n", argc - 1);
+	int i;
+
+	if (argc == 1)
+		printf("%d\n", argc - 1);
+	else
+	{
+		for (i = 0; *argv; i++, argv++)
+			;
+
+		printf("%d\n", i - 1);
+	}
 
 	return (0);
 }
