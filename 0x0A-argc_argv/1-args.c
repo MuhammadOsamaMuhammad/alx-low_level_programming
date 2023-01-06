334f490739d258321d4d5b8440bff73037e64f86
MuhammadOsamaMuhammad <mohamedosama1810@gmail.com>
2023-01-06 14:56:39 -0800

    tse

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,6 +1,13 @@
 #include "main.h"
 #include <stdio.h>
 
+/*
+ * main - entry point
+ * @argc: number of param
+ * @argv: the param
+ * Return: return 0
+ */
+
 int main(int argc, char *argv[])
 {
 	int i;
