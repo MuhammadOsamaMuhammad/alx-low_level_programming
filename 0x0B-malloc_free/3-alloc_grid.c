0b893e28ee6be2e99ec5bdb73e92530c709245fc
MuhammadOsamaMuhammad <105750421+MuhammadOsamaMuhammad@users.noreply.github.com>
2023-01-09 22:16:43 +0200

    Update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,51 +1,46 @@
-#include "main.h"
+#include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
  * alloc_grid - Entry point
- *
- * @width: rows
- *
- * @height: columns
- *
- * Return: pointer to 2D array in the memory or NULL
+ *@width: rows of matrix
+ *@height: columns of string
+ * Return: a pointer to a 2 dimensional array of integers or null
  */
-
 int **alloc_grid(int width, int height)
 {
-	int **arr;
+	int **matrix;
 	int i;
 	int j;
 	int l;
-	int *a;
-
+	int *p;
 
 	if (width <= 0 || height <= 0)
-	{
 		return (NULL);
-	}
-	arr = (int **)malloc(height * sizeof(int *));
+	matrix = (int **)malloc(height * sizeof(int *));
+	if (matrix == NULL)
+		return (NULL);
 	for (i = 0; i < height; i++)
 	{
-		if (*(arr + i) == NULL)
+		*(matrix + i) = (int *)malloc(width * sizeof(int));
+		if (*(matrix + i) == NULL)
 		{
 			for (i = 0; i < height; i++)
 			{
-				a = arr[i];
-				free(a);
+				p = matrix[i];
+				free(p);
 			}
-			free(arr);
+			free(matrix);
 			return (NULL);
 		}
 	}
-
 	for (l = 0; l < height; l++)
 	{
-		for (j = 0 ; j < width; j++)
+		for (j = 0; j < width; j++)
 		{
-			arr[l][j] = 0;
+			matrix[l][j] = 0;
 		}
 	}
-	return (arr);
+	return (matrix);
 }
