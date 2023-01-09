33574e090e1faf8077c501d989266a895eb5b411
MuhammadOsamaMuhammad <mohamedosama1810@gmail.com>
2023-01-09 11:56:06 -0800

    test

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - Entry point
+ *
+ * @s1: first string
+ *
+ * @s2: second string
+ *
+ * Return: pointer to the address in the memory or NULL
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *strnew = NULL;
+	unsigned int i;
+	int n;
+	int j;
+	int c;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	for (n = 0; s1[n] != '\0'; n++)
+		;
+	for (j = 0; s2[j] != '\0'; j++)
+		;
+	strnew = (char *)malloc((n + j + 1) * sizeof(char));
+
+	if (strnew != NULL)
+	{
+		for (i = 0;  s1[i] != '\0'; i++)
+		{
+			strnew[i] = s1[i];
+		}
+		for (c = 0; s2[c] != '\0'; c++)
+		{
+			strnew[i];
+			i++;
+		}
+	} else
+	{
+		return (NULL);
+	}
+	return (strnew);
+}
