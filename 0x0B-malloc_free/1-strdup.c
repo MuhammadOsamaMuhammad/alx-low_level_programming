8b595eec14b1abb3f30679b126c201977dbf4cd7
MuhammadOsamaMuhammad <mohamedosama1810@gmail.com>
2023-01-09 11:39:41 -0800

    test

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - Entry point
+ *
+ * @str: string will be duplicated
+ *
+ * Return: pointer to the address in the memory or NULL
+ */
+
+char *_strdup(char *str)
+{
+	char *strnew = NULL;
+	unsigned int i;
+	int n;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	for (n = 0; str[n] != '\0'; n++)
+		;
+
+	strnew = (char *)malloc(n + 1 * sizeof(char));
+
+
+	if (strnew != NULL)
+	{
+		for (i = 0;  str[i] != '\0'; i++)
+		{
+			strnew[i] = str[i];
+		}
+	}
+
+	strnew[i] = '\0';
+
+	return (array);
+}
