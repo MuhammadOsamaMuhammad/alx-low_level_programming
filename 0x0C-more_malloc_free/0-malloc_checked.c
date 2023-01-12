f967d69fb2ae427328fa0203660f9dea7e0768b8
MuhammadOsamaMuhammad <mohamedosama1810@gmail.com>
2023-01-12 06:02:07 -0800

    tet

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,13 +3,12 @@
 /**
  * malloc_checked - allocates memory.
  * @b: the size to allocate.
- *
  * Return: pointer to allocated memory or 98 if failed.
  */
 
 void *malloc_checked(unsigned int b)
 {
-	char *ptr;
+	void *ptr;
 
 	ptr = malloc(b);
 	if (ptr == NULL)
