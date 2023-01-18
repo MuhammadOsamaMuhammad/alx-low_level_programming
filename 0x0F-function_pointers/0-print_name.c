ee6c00adecdea16722505d598df8a93205be7634
MuhammadOsamaMuhammad <mohamedosama1810@gmail.com>
2023-01-18 09:12:51 -0800

    test

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,16 +1,15 @@
 #include "function_pointers.h"
 
 /**
-  * print_name - initializes a variable
+  * print_name - entry point
   * @name: name
   * @f: pointer
-  * Return: nothing
+  * Return: void
   */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
+	if (name && f)
 	{
-		return;
-	}
 	f(name);
+	}
 }
