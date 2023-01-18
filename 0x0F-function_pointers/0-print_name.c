d17744c108374c564d8fcc83a0085761dcc5429b
MuhammadOsamaMuhammad <105750421+MuhammadOsamaMuhammad@users.noreply.github.com>
2023-01-18 18:17:32 +0200

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,20 +1,13 @@
 #include "function_pointers.h"
-#include <stdio.h>
-
-/*
- * print_name - Entry point
+#include <stdlib.h>
+/**
+ * print_name - prints the name
  * @name: name
  * @f: function
- * Return: void
  */
-
-
-
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
-	{
 		return;
-	}
 	f(name);
 }
