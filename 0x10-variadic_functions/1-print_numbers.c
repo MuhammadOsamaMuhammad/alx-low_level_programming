638f2a012006c1c850ac5457011312190dda8462
MuhammadOsamaMuhammad <105750421+MuhammadOsamaMuhammad@users.noreply.github.com>
2023-01-19 12:40:50 +0200

    Update 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,31 +1,28 @@
-#include "variadic_functions"
-#include <stdrag.h>
+#include "variadic_functions.h"
+#include <stdarg.h>
 #include <stdio.h>
-
-/*
- * print_numbers - print number followed by a new line
- * @n: numbers to print
+/**
+ * print_numbers - Entry Point
+ * @separator: comma space
+ * @n: elements to be printed
  * Return: void
  */
-
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	char *sep;
 	unsigned int i;
-	va_list l;
+	va_list list;
 
 	if (separator == NULL || *separator == 0)
 		sep = "";
 	else
 		sep = (char *) separator;
-
-	va_start(l, n);
+	va_start(list, n);
 
 	if (n > 0)
-		printf("%d", ca_arg(l, int));
-
-	for (i = 0; i < n; i++)
-		printf("%s%d", sep, vaarg(l, int));
+		printf("%d", va_arg(list, int));
+	for (i = 1; i < n; i++)
+		printf("%s%d", sep, va_arg(list, int));
 	printf("\n");
-	va_end(l);
+	va_end(list);
 }
