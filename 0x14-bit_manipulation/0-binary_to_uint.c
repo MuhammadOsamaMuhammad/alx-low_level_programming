a5025c0ac96b2a5bc0818129221a0a3b4c6861c2
MuhammadOsamaMuhammad <mohamedosama1810@gmail.com>
2023-02-02 04:21:14 -0800

    test

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,10 +9,12 @@
  * -> there is one or more chars in the string b that is not 0 or 1
  * -> b is NULL
  */
+
+
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int in = 0;
 	int i;
+	unsigned int dec_val = 0;
 
 	if (!b)
 		return (0);
@@ -20,7 +22,7 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		in = 2 * in + (b[i] - '0');
+		dec_val = 2 * dec_val + (b[i] - '0');
 	}
-	return (in);
+	return (dec_val);
 }
