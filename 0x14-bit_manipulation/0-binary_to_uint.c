f7fb8277a58ed5fb17a8d3922411f82697312047
MuhammadOsamaMuhammad <105750421+MuhammadOsamaMuhammad@users.noreply.github.com>
2023-02-02 14:02:46 +0200

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,18 +11,23 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int value = 0;
+	unsigned int num = 0, mult = 1;
+	int len;
 
-	if (b == NULL)
+	if (b == '\0')
 		return (0);
-	while (*b != '\0')
+
+	for (len = 0; b[len];)
+		len++;
+
+	for (len -= 1; len >= 0; len--)
 	{
-		value = value << 1;
-		if (*b != '1' && *b != '0')
+		if (b[len] != '0' && b[len] != '1')
 			return (0);
-		else if (*b == '1')
-			value = value | 1;
-		b++;
+
+		num += (b[len] - '0') * mult;
+		mult *= 2;
 	}
-	return (value);
+
+	return (num);
 }
