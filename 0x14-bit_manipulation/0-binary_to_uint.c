5c4590a31bb989a2531491f05a14e60f328d27a1
MuhammadOsamaMuhammad <mohamedosama1810@gmail.com>
2023-02-02 04:15:59 -0800

    test

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -18,9 +18,9 @@ unsigned int binary_to_uint(const char *b)
 		return (0);
 	for (i = 0; b[i]; i++)
 	{
-		if (b[i] < '0' || b[i] > 1)
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		in = 2 * in + (b[i] - 0);
+		in = 2 * in + (b[i] - '0');
 	}
 	return (in);
 }
